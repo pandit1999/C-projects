e906986eaeeb2af97d47a444e922b827a7efb3d5
pandit1999 <100330830+pandit1999@users.noreply.github.com>
2022-02-24 09:28:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sanitize2
+{
+    internal class Program
+    {
+        public static string removechar(string str,int n)
+        {
+            return str.Remove(n, 1);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the string:");
+            Console.WriteLine(removechar("c#.net", 2));
+            Console.ReadLine();
+
+        }
+    }
+}
